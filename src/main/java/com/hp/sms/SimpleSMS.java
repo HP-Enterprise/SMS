ffd912558c52d3d865706f1aaceadc473788c8ac
AceBear <guang-hua.xiong@hp.com>
2015-10-21 18:07:50 +0800

    remove unused import

--- src/main/java/com/hp/sms/SimpleSMS.java
+++ src/main/java/com/hp/sms/SimpleSMS.java
@@ -1,5 +1,4 @@
 package com.hp.sms;
-import com.sun.javafx.image.BytePixelSetter;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
